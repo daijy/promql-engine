e0639bc9b686a57fb61549e9c9a78c13ae694f13
Daniel Dai <jidai@roku.com>
2025-09-11 19:41:38 -0700

    Better logging

--- execution/aggregate/hashaggregate.go
+++ execution/aggregate/hashaggregate.go
@@ -134,7 +134,6 @@ func (a *aggregate) Next(ctx context.Context) ([]model.StepVector, error) {
 
 	log.SetFlags(0)
 	log.SetOutput(new(logWriter))
-	log.Printf("jidai5: here0")
 	var err error
 	a.once.Do(func() { err = a.initializeTables(ctx) })
 	if err != nil {
@@ -142,6 +141,7 @@ func (a *aggregate) Next(ctx context.Context) ([]model.StepVector, error) {
 	}
 
 	if a.paramOp != nil {
+		log.Printf("jidai5: here0")
 		args, err := a.paramOp.Next(ctx)
 		if err != nil {
 			return nil, err
@@ -156,7 +156,6 @@ func (a *aggregate) Next(ctx context.Context) ([]model.StepVector, error) {
 		a.paramOp.GetPool().PutVectors(args)
 	}
 
-	log.Printf("jidai5: params %d", len(a.params))
 	for i, p := range a.params {
 		a.tables[i].reset(p)
 	}
@@ -189,7 +188,6 @@ func (a *aggregate) Next(ctx context.Context) ([]model.StepVector, error) {
 		break
 	}
 
-	log.Printf("jidai5: here4")
 	if a.tables[0].timestamp() == math.MinInt64 {
 		return nil, nil
 	}
