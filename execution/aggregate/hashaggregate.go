f10aa7696e4caa58c2b10b1966a0886895cba77b
Daniel Dai <jidai@roku.com>
2025-09-11 19:36:04 -0700

    Better logging

--- execution/aggregate/hashaggregate.go
+++ execution/aggregate/hashaggregate.go
@@ -114,6 +114,13 @@ func (a *aggregate) GetPool() *model.VectorPool {
 	return a.vectorPool
 }
 
+type logWriter struct {
+}
+
+func (writer logWriter) Write(bytes []byte) (int, error) {
+	return fmt.Print(time.Now().UTC().Format("2006-01-02T15:04:05.999Z") + string(bytes))
+}
+
 func (a *aggregate) Next(ctx context.Context) ([]model.StepVector, error) {
 	log.Printf("jidai start aggregate")
 	start := time.Now()
@@ -125,6 +132,8 @@ func (a *aggregate) Next(ctx context.Context) ([]model.StepVector, error) {
 	default:
 	}
 
+	log.SetFlags(0)
+	log.SetOutput(new(logWriter))
 	log.Printf("jidai5: here0")
 	var err error
 	a.once.Do(func() { err = a.initializeTables(ctx) })
