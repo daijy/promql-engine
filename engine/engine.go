450430d98be56cd0da5e2236a2378e26230fd294
Daniel Dai <jidai@roku.com>
2025-09-13 23:32:27 -0700

    Better logging

--- engine/engine.go
+++ engine/engine.go
@@ -616,7 +616,6 @@ loop:
 
 	// For range Query we expect always a Matrix value type.
 	if q.t == RangeQuery {
-		log.Printf("daijy8: len(series), %d", len(series))
 		matrix := make(promql.Matrix, 0, len(series))
 		for _, s := range series {
 			if len(s.Floats)+len(s.Histograms) == 0 {
@@ -631,6 +630,7 @@ loop:
 		}
 		return ret
 	}
+	log.Printf("daijy8: len(series), %d", len(series))
 
 	var result parser.Value
 	switch q.plan.Root().ReturnType() {
