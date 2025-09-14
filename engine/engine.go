84982730577dadb2ca72138f8fe4d826634cac1a
Daniel Dai <jidai@roku.com>
2025-09-14 13:54:41 -0700

    Better logging

--- engine/engine.go
+++ engine/engine.go
@@ -575,7 +575,9 @@ loop:
 			return newErrResult(ret, ctx.Err())
 		default:
 			r, err := q.Query.exec.Next(ctx)
-			log.Printf("daijy7 len(r), %d, %d, %d", len(r), len(r[0].SampleIDs), len(r[0].HistogramIDs))
+			if r != nil {
+				log.Printf("daijy7 len(r), %d, %d, %d", len(r), len(r[0].SampleIDs), len(r[0].HistogramIDs))
+			}
 			if err != nil {
 				return newErrResult(ret, err)
 			}
