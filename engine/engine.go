2e0f82d6b8c933fa88a79bd19e2dffa2f1bd9138
Daniel Dai <jidai@roku.com>
2025-09-15 11:24:03 -0700

    Better logging

--- engine/engine.go
+++ engine/engine.go
@@ -9,6 +9,7 @@ import (
 	"log/slog"
 	"math"
 	"runtime"
+	"runtime/debug"
 	"slices"
 	"sort"
 	"time"
@@ -439,6 +440,7 @@ func (e *Engine) NewInstantQuery(ctx context.Context, q storage.Queryable, opts 
 
 // NewRangeQuery implements the promql.Engine interface.
 func (e *Engine) NewRangeQuery(ctx context.Context, q storage.Queryable, opts promql.QueryOpts, qs string, start, end time.Time, step time.Duration) (promql.Query, error) {
+	debug.PrintStack()
 	log.Printf("daijy enter thanos query %T", q)
 	return e.MakeRangeQuery(ctx, q, fromPromQLOpts(opts), qs, start, end, step)
 }
