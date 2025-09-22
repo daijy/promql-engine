b4f8931295c8b463cd3e9babf53ba85df76ea4c1
Daniel Dai <jidai@roku.com>
2025-09-23 05:01:38 +0800

    Better logging

--- storage/prometheus/series_selector.go
+++ storage/prometheus/series_selector.go
@@ -75,8 +75,8 @@ func (o *seriesSelector) loadSeries(ctx context.Context) error {
 }
 
 func seriesShard(series []SignedSeries, index int, numShards int) []SignedSeries {
-	var series_counter int = 0
 	for _, s := range series {
+		var series_counter int = 0
 		it := s.Series.Iterator(nil)
 		for it.Next() != 0 {
 			series_counter++
