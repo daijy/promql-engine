382df2f007e6f9b3a0f911167b5f9bc77a5bc583
Daniel Dai <jidai@roku.com>
2025-09-23 15:25:55 +0800

    Better logging

--- logicalplan/passthrough.go
+++ logicalplan/passthrough.go
@@ -4,7 +4,7 @@
 package logicalplan
 
 import (
-	"log"
+	"runtime/debug"
 
 	"github.com/thanos-io/promql-engine/api"
 	"github.com/thanos-io/promql-engine/query"
@@ -45,7 +45,7 @@ func matchingEngineTime(e api.RemoteEngine, opts *query.Options) bool {
 }
 
 func (m PassthroughOptimizer) Optimize(plan Node, opts *query.Options) (Node, annotations.Annotations) {
-	log.Print("PassthroughOptimizer")
+	debug.PrintStack()
 	engines := m.Endpoints.Engines()
 	if len(engines) == 1 {
 		if !matchingEngineTime(engines[0], opts) {
