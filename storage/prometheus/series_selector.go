4b43afa2d20ba1f256bb615c9470b244b33632e3
Daniel Dai <jidai@roku.com>
2025-09-26 10:53:56 +0800

    Revert "Better logging"
    This reverts commit fc511bdebf77cf45ceeb364bc7dcc9bc4706dad3.

--- storage/prometheus/series_selector.go
+++ storage/prometheus/series_selector.go
@@ -76,11 +76,9 @@ func (o *seriesSelector) loadSeries(ctx context.Context) error {
 
 func seriesShard(series []SignedSeries, index int, numShards int) []SignedSeries {
 	it := series[540118].Iterator(nil)
-	log.Printf("total series count: %d", len(series))
-	log.Print(series[540118].Labels().String())
 	for it.Next() != 0 {
 		ts, v := it.At()
-		log.Printf("%d = %f", ts, v)
+		log.Printf("series %s: %d = %f", series[540118].Labels().String(), ts, v)
 	}
 
 	start := index * len(series) / numShards
